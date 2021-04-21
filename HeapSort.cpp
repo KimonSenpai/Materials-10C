90bbe0261a4668fe23db2fb2aca22d84f1d07ae7
Kim <kim.zyong@yandex.ru>
2021-04-21 14:36:36 +0300

    --

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -6,7 +6,7 @@ void PushHeap(int* mas, int& n, int val) {
     n++;
     int i = n - 1;
     while (i != 0) {
-        if (mas[i] < mas[(i + 1) / 2 - 1]) {
+        if (mas[i] > mas[(i + 1) / 2 - 1]) {
             swap(mas[i], mas[(i + 1) / 2 - 1]);
             i = (i + 1) / 2 - 1;
         } else {
@@ -17,7 +17,7 @@ void PushHeap(int* mas, int& n, int val) {
 
 int PopHeap(int* mas, int& n) {
     int v = mas[0];
-    mas[0] = mas[n - 1];
+    swap(mas[0], mas[n - 1]);
     n--;
     int i = 0;
     while (true) {
@@ -25,15 +25,15 @@ int PopHeap(int* mas, int& n) {
             l = 2 * i + 1, 
             r = 2 * i + 2;
 
-            
+
         if (l >= n) break;
         if (r >= n) {
-            if (mas[i] > mas[l]) swap(mas[i], mas[l]);
+            if (mas[i] < mas[l]) swap(mas[i], mas[l]);
             break;
         }
-        if (mas[l] > mas[r]) swap(l, r);
+        if (mas[l] < mas[r]) swap(l, r);
 
-        if (mas[i] > mas[l]) {
+        if (mas[i] < mas[l]) {
             swap(mas[i], mas[l]);
             i = l;
         } else {
@@ -45,13 +45,13 @@ int PopHeap(int* mas, int& n) {
 
 void HeapSort(int* beg, int* end) {
     int n = end - beg;
-    int* heap = new int[n];
+    //int* heap = new int[n];
     int count = 0;
     for (int* p = beg; p < end; p++) {
-        PushHeap(heap, count, *p);
+        PushHeap(beg, count, *p);
     }
     for (int* p = beg; p < end; p++) {
-        *p = PopHeap(heap, count);
+        PopHeap(beg, count);
     }
 }
 
